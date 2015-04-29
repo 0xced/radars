63e523f8d8838f9cfeed6dee3b84cbbd22c936af
Cédric Luthi <cedric.luthi@gmail.com>
2015-04-30 00:11:17 +0200

    Demo app that demonstrates the crash introduced in iOS 8.4

--- KVO externalPlaybackActive crash/KVO externalPlaybackActive crash/ViewController.m
+++ KVO externalPlaybackActive crash/KVO externalPlaybackActive crash/ViewController.m
@@ -5,6 +5,46 @@
 
 #import "ViewController.h"
 
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UIView *videoView;
+
+@property (nonatomic) MPMoviePlayerController *moviePlayerController;
+
+@end
+
 @implementation ViewController
+
+- (IBAction) playBasicStream:(id)sender
+{
+	NSURL *streamURL = [NSURL URLWithString:@"https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8"];
+	[self playStreamURL:streamURL];
+}
+
+- (IBAction) playAdvancedStream:(id)sender
+{
+	NSURL *streamURL = [NSURL URLWithString:@"https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"];
+	[self playStreamURL:streamURL];
+}
+
+- (void) playStreamURL:(NSURL *)streamURL
+{
+	self.moviePlayerController.contentURL = streamURL;
+	[self.moviePlayerController play];
+}
+
+- (MPMoviePlayerController *) moviePlayerController
+{
+	if (!_moviePlayerController)
+	{
+		_moviePlayerController = [MPMoviePlayerController new];
+		_moviePlayerController.view.frame = CGRectMake(0.f, 0.f, self.videoView.bounds.size.width, self.videoView.bounds.size.height);
+		_moviePlayerController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+		[self.videoView addSubview:_moviePlayerController.view];
+	}
+	return _moviePlayerController;
+}
 
 @end
